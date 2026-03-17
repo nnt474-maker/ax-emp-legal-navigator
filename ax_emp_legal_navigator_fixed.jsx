a7ca32c9f9cc816d7df2897d3f281f883292a12b
nnt474-maker <nnt474@gmail.com>
2026-03-17 17:15:50 +0700

    Add files via upload

---
+++ ax_emp_legal_navigator_fixed.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import LegalEmploymentNavigatorApp from "./src/App.jsx";
+
+export default function AXEMPLegalNavigatorFixed() {
+  return <LegalEmploymentNavigatorApp />;
+}
