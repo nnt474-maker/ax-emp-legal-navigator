297b504b856822648db2af3332038d70a7c33c89
nnt474-maker <nnt474@gmail.com>
2026-03-17 17:13:46 +0700

    Delete ax_emp_legal_navigator_v3.jsx

--- ax_emp_legal_navigator_v3.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LegalEmploymentNavigatorApp from "./src/App.jsx";
-
-export default function AXEMPLegalNavigatorV3() {
-  return <LegalEmploymentNavigatorApp />;
-}
